2f22ee20cb03fd14460e037f2ebbcd4ec72f657a
Udit Shukla <75034362+Udit-Shukla@users.noreply.github.com>
2024-06-17 13:56:19 +0530

    Add files via upload

---
+++ components/Grid.tsx
@@ -0,0 +1,27 @@
+import { gridItems } from "@/data"
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+
+const Grid = () => {
+  return (
+    <section id="about">
+        <BentoGrid>
+            {gridItems.map((item)=>(
+                <BentoGridItem 
+                id={item.id}
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                className={item.className}
+                imgClassName={item.imgClassName}
+                titleClassName={item.titleClassName}
+                img={item.img}
+                spareImg={item.spareImg}
+                />
+
+            ))}
+        </BentoGrid> 
+    </section>
+  )
+}
+
+export default Grid
